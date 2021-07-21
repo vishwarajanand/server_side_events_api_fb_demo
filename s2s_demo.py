c2f23ef0fafe2a42d7b12c9ba324a5e6edd252f3
Vishwaraj Anand <vishwarajanand@fb.com>
2021-07-21 21:12:27 +0800

    Rectifying payload and reformatting

--- s2s_demo.py
+++ s2s_demo.py
@@ -9,36 +9,33 @@ FacebookAdsApi.init(app_id, app_secret, access_token)
 fields = []
 
 params = {
-    "data": [{
-        "event_name": "Purchase",
-        # Unix timestamp example: 1598927035
-        "event_time": int(time.time()),
-        "event_id": 'event.id.12346',
-        "event_source_url":
-        "http://jaspers-market.com/product/some_product_url",
-        "user_data": {
-            "client_ip_address":
-            "2620:10d:c094:200::1:9c87",
-            "client_user_agent":
-            "demo s2s ua",
-            "em": [
-                "309a0a5c3e211326ae75ca18196d301a9bdbd1a882a4d2569511033da23f0abd"
-            ],
-            "fbc":
-            "fb.1.1554763741205.AbCdEfGhIjKlMnOpQrStUvWxYz1234567890",
-            "fbp":
-            "fb.1.1558571054389.1098115397"
-        },
-        "custom_data": {
-            "value": 100.2,
-            "currency": "USD",
-            "content_ids": ["product.id.123"],
-            "content_type": "product"
-        },
-        "data_processing_options": [],
-        "action_source": "website",
-        "opt_out": False
-    }]
+    "data": [
+        {
+            "event_name": "Purchase",
+            # Unix timestamp example: 1598927035
+            "event_time": int(time.time()),
+            "event_id": "event.id.12346",
+            "event_source_url": "http://jaspers-market.com/product/some_product_url",
+            "user_data": {
+                "client_ip_address": "2620:10d:c094:200::1:9c87",
+                "client_user_agent": "demo s2s ua",
+                "em": [
+                    "309a0a5c3e211326ae75ca18196d301a9bdbd1a882a4d2569511033da23f0abd"
+                ],
+                "fbc": "fb.1.1554763741205.AbCdEfGhIjKlMnOpQrStUvWxYz1234567890",
+                "fbp": "fb.1.1558571054389.1098115397",
+            },
+            "custom_data": {
+                "value": 100.2,
+                "currency": "USD",
+                "content_ids": ["product.id.123"],
+                "content_type": "product",
+            },
+            "data_processing_options": [],
+            "action_source": "website",
+            "opt_out": "false",
+        }
+    ]
 }
 
 # to see events in events manager's test live events tool
